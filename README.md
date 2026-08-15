b4ffc3283d8b5ff4bcff4eb00a719cf768efba2c
salvvage <tenslyt@gmail.com>
2026-08-15 17:43:28 +0200

    Update

---
+++ README.md
@@ -0,0 +1,26 @@
+# I use oxwm, btw.
+## https://github.com/tonybanters/oxwm
+My dotfiles are very simple and they don't really have anything "new" in them. They aim to be as minimalistic as possible.
+
+# Dependencies
+Note: These are NOT required. You can still use my dotfiles, with whatever terminal/compositor/wallpaper manager you want. Just keep in mind that you have to edit config.lua then!  
+  
+Flameshot (Screenshot App)  
+Alacritty (Terminal)  
+Picom (Compositor)  
+xwallpaper (Wallpapers)  
+
+
+# Keybinds
+Super + D -> DMenu  
+Super + Q -> Kill focused window  
+Super + Print -> Screenshot (Flameshot)  
+Super + 1-9 -> Switch workspaces  
+Super + Enter -> Terminal
+
+# Violet 
+<img width="1901" height="1079" alt="2026-08-11_09-27" src="https://github.com/user-attachments/assets/b9f9afc3-bde6-48d1-9fdb-f287725f8424" />
+  
+# Gentoo Dark
+
+<img width="1919" height="1079" alt="2026-08-15_16-12" src="https://github.com/user-attachments/assets/dab50cde-3578-44c4-8498-f08db88cf2d5" />
---
+++ gentoo_dark/oxwm/config.lua
@@ -0,0 +1,182 @@
+---@meta
+---@module 'oxwm'
+---
+local modkey = "Mod4"
+local terminal = "alacritty"
+
+local colors = {
+    bg = "#000000",
+    fg = "#d4d4d4",
+    red = "#f14c4c",
+    cyan = "#9cdcfe",
+    green = "#6a9955",
+    light_blue = "#4ec9b0",
+    blue = "#569cd6",
+    purple = "#c586c0",
+    yellow = "#dcdcaa",
+    orange = "#ce9178",
+    grey = "#808080",
+    sep = "#d2d2d2",
+    white = "#ffffff",
+    dvio1 = "#4b357a",
+    dvio2 = "#1c1a2e",
+    dvio3 = "#6e5ac2",
+    dvio4 = "#341953"
+}
+
+local tags = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }
+-- local tags = { "", "󰊯", "", "󰰏", "󰟿", "󱇤", "", "󱘶", "󰧮" } -- Example of nerd font icon tags
+
+local bar_font = "JetBrainsMono:style=Bold:size=12"
+
+local blocks = {
+	oxwm.bar.block.datetime({
+        --format = "󰸘 {}",
+        date_format = "%-I:%M %P",
+        interval = 1,
+        color = colors.dvio3,
+        underline = false,
+    }),
+        oxwm.bar.block.systray({
+    }) 
+};
+
+-------------------------------------------------------------------------------
+-- Basic Settings
+-------------------------------------------------------------------------------
+oxwm.set_terminal(terminal)
+oxwm.set_modkey(modkey)
+oxwm.set_tags(tags)
+
+-------------------------------------------------------------------------------
+-- Layouts
+-------------------------------------------------------------------------------
+oxwm.set_layout_symbol("tiling", "[T]")
+oxwm.set_layout_symbol("normie", "[F]")
+oxwm.set_layout_symbol("tabbed", "[=]")
+
+-------------------------------------------------------------------------------
+-- Appearance
+-------------------------------------------------------------------------------
+oxwm.border.set_width(1)
+oxwm.border.set_focused_color(colors.dvio4)
+oxwm.border.set_unfocused_color(colors.dvio2)
+
+-- Smart Enabled = No border if 1 window
+oxwm.gaps.set_smart(true)
+oxwm.gaps.set_inner(5, 5)
+oxwm.gaps.set_outer(5, 5)
+
+
+oxwm.bar.set_font(bar_font)
+oxwm.bar.set_blocks(blocks)
+oxwm.bar.set_scheme_normal(colors.dvio1, "#080711")
+oxwm.bar.set_scheme_occupied(colors.dvio3)
+oxwm.bar.set_scheme_selected(colors.white,"#3b2e46",colors.white)
+
+oxwm.key.bind({ modkey }, "Return", oxwm.spawn_terminal())
+-- oxwm.key.bind({ modkey }, "D", oxwm.spawn({ "sh", "-c", "dmenu_run -l 10" }))
+oxwm.key.bind({ modkey }, "D", oxwm.spawn({ "sh", "-c", "rofi -show drun" }))
+oxwm.key.bind({ modkey }, "Print", oxwm.spawn({ "sh", "-c", "flameshot gui" }))
+oxwm.key.bind({ modkey }, "Q", oxwm.client.kill())
+
+oxwm.key.bind({ modkey, "Shift" }, "Slash", oxwm.show_keybinds())
+
+oxwm.key.bind({ modkey, "Shift" }, "F", oxwm.client.toggle_fullscreen())
+oxwm.key.bind({ modkey, "Shift" }, "Space", oxwm.client.toggle_floating())
+
+oxwm.key.bind({ modkey }, "C", oxwm.layout.set("tiling"))
+oxwm.key.bind({ modkey }, "N", oxwm.layout.cycle())
+
+oxwm.key.bind({ modkey }, "H", oxwm.set_master_factor(-5))
+oxwm.key.bind({ modkey }, "L", oxwm.set_master_factor(5))
+oxwm.key.bind({ modkey }, "I", oxwm.inc_num_master(1))
+oxwm.key.bind({ modkey }, "P", oxwm.inc_num_master(-1))
+
+oxwm.key.bind({ modkey }, "A", oxwm.toggle_gaps())
+
+oxwm.key.bind({ modkey, "Shift" }, "Q", oxwm.quit())
+oxwm.key.bind({ modkey, "Shift" }, "R", oxwm.restart())
+
+oxwm.key.bind({ modkey }, "J", oxwm.client.focus_stack(1))
+oxwm.key.bind({ modkey }, "K", oxwm.client.focus_stack(-1))
+
+oxwm.key.bind({ modkey, "Shift" }, "J", oxwm.client.move_stack(1))
+oxwm.key.bind({ modkey, "Shift" }, "K", oxwm.client.move_stack(-1))
+
+oxwm.key.bind({ modkey }, "Comma", oxwm.monitor.focus(-1))
+oxwm.key.bind({ modkey }, "Period", oxwm.monitor.focus(1))
+oxwm.key.bind({ modkey, "Shift" }, "Comma", oxwm.monitor.tag(-1))
+oxwm.key.bind({ modkey, "Shift" }, "Period", oxwm.monitor.tag(1))
+
+oxwm.key.bind({ modkey }, "1", oxwm.tag.view(0))
+oxwm.key.bind({ modkey }, "2", oxwm.tag.view(1))
+oxwm.key.bind({ modkey }, "3", oxwm.tag.view(2))
+oxwm.key.bind({ modkey }, "4", oxwm.tag.view(3))
+oxwm.key.bind({ modkey }, "5", oxwm.tag.view(4))
+oxwm.key.bind({ modkey }, "6", oxwm.tag.view(5))
+oxwm.key.bind({ modkey }, "7", oxwm.tag.view(6))
+oxwm.key.bind({ modkey }, "8", oxwm.tag.view(7))
+oxwm.key.bind({ modkey }, "9", oxwm.tag.view(8))
+
+oxwm.key.bind({ modkey, "Shift" }, "1", oxwm.tag.move_to(0))
+oxwm.key.bind({ modkey, "Shift" }, "2", oxwm.tag.move_to(1))
+oxwm.key.bind({ modkey, "Shift" }, "3", oxwm.tag.move_to(2))
+oxwm.key.bind({ modkey, "Shift" }, "4", oxwm.tag.move_to(3))
+oxwm.key.bind({ modkey, "Shift" }, "5", oxwm.tag.move_to(4))
+oxwm.key.bind({ modkey, "Shift" }, "6", oxwm.tag.move_to(5))
+oxwm.key.bind({ modkey, "Shift" }, "7", oxwm.tag.move_to(6))
+oxwm.key.bind({ modkey, "Shift" }, "8", oxwm.tag.move_to(7))
+oxwm.key.bind({ modkey, "Shift" }, "9", oxwm.tag.move_to(8))
+
+oxwm.key.bind({ modkey, "Control" }, "1", oxwm.tag.toggleview(0))
+oxwm.key.bind({ modkey, "Control" }, "2", oxwm.tag.toggleview(1))
+oxwm.key.bind({ modkey, "Control" }, "3", oxwm.tag.toggleview(2))
+oxwm.key.bind({ modkey, "Control" }, "4", oxwm.tag.toggleview(3))
+oxwm.key.bind({ modkey, "Control" }, "5", oxwm.tag.toggleview(4))
+oxwm.key.bind({ modkey, "Control" }, "6", oxwm.tag.toggleview(5))
+oxwm.key.bind({ modkey, "Control" }, "7", oxwm.tag.toggleview(6))
+oxwm.key.bind({ modkey, "Control" }, "8", oxwm.tag.toggleview(7))
+oxwm.key.bind({ modkey, "Control" }, "9", oxwm.tag.toggleview(8))
+
+oxwm.key.bind({ modkey, "Control", "Shift" }, "1", oxwm.tag.toggletag(0))
+oxwm.key.bind({ modkey, "Control", "Shift" }, "2", oxwm.tag.toggletag(1))
+oxwm.key.bind({ modkey, "Control", "Shift" }, "3", oxwm.tag.toggletag(2))
+oxwm.key.bind({ modkey, "Control", "Shift" }, "4", oxwm.tag.toggletag(3))
+oxwm.key.bind({ modkey, "Control", "Shift" }, "5", oxwm.tag.toggletag(4))
+oxwm.key.bind({ modkey, "Control", "Shift" }, "6", oxwm.tag.toggletag(5))
+oxwm.key.bind({ modkey, "Control", "Shift" }, "7", oxwm.tag.toggletag(6))
+oxwm.key.bind({ modkey, "Control", "Shift" }, "8", oxwm.tag.toggletag(7))
+oxwm.key.bind({ modkey, "Control", "Shift" }, "9", oxwm.tag.toggletag(8))
+
+oxwm.key.chord({
+    { { modkey }, "Space" },
+    { {},         "T" }
+}, oxwm.spawn_terminal())
+
+oxwm.key.chord({
+    { { modkey }, "F" },
+    { {},         "B" }
+}, oxwm.spawn({ "sh", "-c", "$HOME/repos/dmenu-scripts/bookmarks-dmenu.sh" }))
+
+oxwm.key.chord({
+    { { modkey }, "F" },
+    { {},         "F" }
+}, oxwm.spawn({ "sh", "-c", "$HOME/repos/dmenu-scripts/repos-dmenu.sh" }))
+
+oxwm.key.chord({
+    { { modkey }, "F" },
+    { {},         "O" }
+}, oxwm.spawn({ "sh", "-c", "$HOME/repos/dmenu-scripts/tmux-dmenu.sh" }))
+
+
+-------------------------------------------------------------------------------
+-- Autostart
+-------------------------------------------------------------------------------
+-- Commands to run once when OXWM starts
+-- Uncomment and modify these examples, or add your own
+
+oxwm.autostart("picom --backend xrender")
+oxwm.autostart("xwallpaper --stretch ~/Pictures/walls/2.png")
+-- oxwm.autostart("dunst")
+-- oxwm.autostart("nm-applet")
